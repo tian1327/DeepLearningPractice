25f9fb57baf0df41d4e425558fe5b4cfe775d01f
Tian Liu <liutian1113@gmail.com>
2021-12-24 18:19:36 -0600

    update ResNet50 script

--- ResNet50_cat-dog/ResNet50.py
+++ ResNet50_cat-dog/ResNet50.py
@@ -1,14 +1,17 @@
-from keras.applications.resnet50 import ResNet50
+from keras.api._v2.keras.applications.resnet50 import ResNet50
+
+# from keras.applications.resnet50 import ResNet50
 from keras.layers import Dense, GlobalAveragePooling2D
 from keras.models import Model
-from keras.optimizers import SGD
+
+from keras.api._v2.keras.optimizers import SGD
+# from keras.optimizers import SGD
 from keras.preprocessing.image import ImageDataGenerator
-import numpy as np
 
 # Download the architecture of ResNet50 with ImageNet weights
 base_model = ResNet50(include_top=False, weights='imagenet')
 
-# Taking the output of the last convolution block in ResNet50
+# Taking the output of the last convoution block in ResNet50
 x = base_model.output
 
 # Adding a Global Average Pooling layer
@@ -52,26 +55,26 @@ test_set = test_datagen.flow_from_directory('test_set',
                                             batch_size = 32,
                                             class_mode = 'categorical')
 
-# Training the model for 5 epochs
-model.fit_generator(training_set,
-                         steps_per_epoch = 250,
-                         epochs = 5,
-                         validation_data = test_set,
-                         validation_steps = 2000)
-
-# We will try to train the last stage of ResNet50
-# for layer in base_model.layers[0:143]:
-#   layer.trainable = False
-
-# for layer in base_model.layers[143:]:
-#   layer.trainable = True
-
-# # Training the model for 10 epochs
+# # Training the model for 5 epochs
 # model.fit_generator(training_set,
-#                          steps_per_epoch = 250,
-#                          epochs = 10,
+#                          steps_per_epoch = 8000,
+#                          epochs = 5,
 #                          validation_data = test_set,
 #                          validation_steps = 2000)
+
+# We will try to train the last stage of ResNet50
+for layer in base_model.layers[0:143]:
+  layer.trainable = False
+
+for layer in base_model.layers[143:]:
+  layer.trainable = True
+
+# Training the model for 10 epochs
+model.fit_generator(training_set,
+                         steps_per_epoch = 8000,
+                         epochs = 10,
+                         validation_data = test_set,
+                         validation_steps = 2000)
 
 # Saving the weights in the current directory
 model.save_weights("resnet50_weights.h5")
@@ -80,7 +83,6 @@ model.save_weights("resnet50_weights.h5")
 import numpy as np
 from keras.preprocessing import image
 test_image = image.load_img('cat_or_dog_test.jpg', target_size = (224, 224))
-# test_image = image.load_img('test.png', target_size = (224, 224))
 test_image = image.img_to_array(test_image)
 
 # Expanding the 3-d image to 4-d image.
@@ -88,17 +90,15 @@ test_image = image.img_to_array(test_image)
 test_image = np.expand_dims(test_image, axis = 0)
 
 # Predicting the final class
+# result = classifier.predict(test_image)
 result = model.predict(test_image)
-print("type of result is",type(result))
-print("result =", result)
 
 # Fetching the class labels
 labels = training_set.class_indices
 labels = list(labels.items())
-print(labels)
 
 # Printing the final label
 for label, i in labels:
-    if i == np.argmax(result):
+    if i == result:
         print("The test image has: ", label)
         break
